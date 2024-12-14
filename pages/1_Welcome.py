7583e46a457dfdea7f0525f76888f47674f28255
MOHAMMED-SAEED-OMER <muhammadsahed55@gmail.com>
2024-12-14 16:03:25 +0300

    Create 1_Welcome.py

---
+++ pages/1_Welcome.py
@@ -0,0 +1,7 @@
+# pages/1_Welcome.py
+import streamlit as st
+
+def show_page():
+    st.title("Welcome!")
+    st.write("This is the home page of the app.")
+    st.write("Use the sidebar to navigate through the app.")
