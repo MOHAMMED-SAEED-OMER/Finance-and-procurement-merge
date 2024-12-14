ddfc0e0941f99b0de5919d0367d546fd881e9777
MOHAMMED-SAEED-OMER <muhammadsahed55@gmail.com>
2024-12-14 16:09:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Welcome", "Submit Request", "Manager View", "Finance View", "All Requests"])
 
 if page == "Welcome":
-    import pages.1_Welcome as welcome
+    import pages.welcome as welcome
     welcome.show_page()
 elif page == "Submit Request":
-    import pages.2_Request as request_page
+    import pages.request as request_page
     request_page.show_page()
 elif page == "Manager View":
-    import pages.3_Manager_View as manager_view
+    import pages.manager_view as manager_view
     manager_view.show_page()
 elif page == "Finance View":
-    import pages.4_Finance_View as finance_view
+    import pages.finance_view as finance_view
     finance_view.show_page()
 elif page == "All Requests":
-    import pages.5_All_Requests as all_requests
+    import pages.all_requests as all_requests
     all_requests.show_page()
